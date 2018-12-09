0d1689f0f31ca9312cbe934bb3ce1bf13eb0b4be
Youness <Youness@DESKTOP-BQ87NOV>
2018-12-09 20:49:26 +0100

    Insertar los getters y setters con las validaciones

--- src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -11,9 +11,45 @@ package org.iesalandalus.programacion.agenda;
  */
 public class Contacto 
 {
-static final String ER_TELEFONO="";
-static final String ER_CORREO="";
+static final String ER_TELEFONO="[69]([0-9]{8})";
+static final String ER_CORREO="([0-9a-zA-Z._-]+@)([a-z]+[.])([a-z])+";
 private String nombre;
 private String telefono;
 private String correo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) 
+    {
+        if(nombre==null && nombre.length()<0)
+            throw new IllegalArgumentException("ERROR: no puedes cambiar el nombre y tampoco puede estar vacio");
+        
+        else this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) 
+    {
+        if(telefono.matches(ER_TELEFONO))
+        this.telefono = telefono;
+        else throw new IllegalArgumentException("ERROR: numero no cumple los requisitos");
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) 
+    {
+        if(correo.matches(ER_CORREO))
+        this.correo = correo;
+        else throw new IllegalArgumentException("ERROR: correo no cumple los requisitos");
+    }
+
+
 }
