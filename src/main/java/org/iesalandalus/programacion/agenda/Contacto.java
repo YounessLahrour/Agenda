2e546cd83aeaf04570c84c286655b93e960b8531
Youness <Youness@DESKTOP-BQ87NOV>
2018-12-09 16:46:44 +0100

    Crear clase Contacto con sus atributos

---
+++ src/main/java/org/iesalandalus/programacion/agenda/Contacto.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.agenda;
+
+/**
+ *
+ * @author Youness
+ */
+public class Contacto 
+{
+static final String ER_TELEFONO="";
+static final String ER_CORREO="";
+private String nombre;
+private String telefono;
+private String correo;
+}
