5f1576fe573272009f8a52e8a0da7ed100c62506
Youness <Youness@DESKTOP-BQ87NOV>
2018-12-10 20:17:17 +0100

    Creo la clase Agenda con sus atributos

---
+++ src/test/java/org/iesalandalus/programacion/agenda/modelo/Agenda.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.iesalandalus.programacion.agenda.modelo;
+
+/**
+ *
+ * @author Youness
+ */
+public class Agenda 
+{
+    private static final int MAX_CONTACTOS=100;
+    private int numContactos;
+    
+}
